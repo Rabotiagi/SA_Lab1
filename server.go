4e6795e7da66e652a74e99744b8c4ee1c5aad3e9
yaroslav_els <yarter003@gmail.com>
2022-02-22 22:16:13 +0200

    implemented handler for /time

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+<<<<<<< HEAD
 	"fmt"
 	"net/http"
 )
@@ -9,3 +10,23 @@ func main() {
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
+=======
+  "encoding/json"
+  "fmt"
+  "net/http"
+  "time"
+)
+
+func main() {
+  http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+    currentTime := time.Now().UTC()
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(200)
+    _ = json.NewEncoder(w).Encode(currentTime.Format("2006-01-02T15:04:05Z07:00"))
+  })
+
+  fmt.Println("Server is running on port 3000")
+  http.ListenAndServe(":3000", nil)
+}
+>>>>>>> 77911b9 (implemented handler for /time)
