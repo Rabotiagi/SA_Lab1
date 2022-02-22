c40ec65b9bada26b7ac1b2048c27febd121a5d6b
yaroslav_els <yarter003@gmail.com>
2022-02-22 23:09:35 +0200

    gofmt formatting

--- server.go
+++ server.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "net/http"
-  "time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func main() {
-  http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-    currentTime := time.Now().UTC()
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := time.Now().UTC()
 
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(200)
-    _ = json.NewEncoder(w).Encode(currentTime.Format("2006-01-02T15:04:05Z07:00"))
-  })
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		_ = json.NewEncoder(w).Encode(currentTime.Format("2006-01-02T15:04:05Z07:00"))
+	})
 
-  fmt.Println("Server is running on port 3000")
-  http.ListenAndServe(":3000", nil)
+	fmt.Println("Server is running on port 3000")
+	http.ListenAndServe(":3000", nil)
 }
